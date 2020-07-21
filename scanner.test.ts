71a348110f90a50490d3074b559abf6f1bae9af4
Vasilii Rogin <vasilii@giosg.com>
2020-07-21 18:45:01 +0300

    Add tests for rolling-back scanner

---
+++ scanner.test.ts
@@ -0,0 +1,194 @@
+import { Token } from "./scanner.func";
+import { Scanner } from "./scanner";
+
+const DEMO_TOKENS = "abcdefghijklmno";
+const DEMO_TOKEN_STREAM: ReadonlyArray<Token> = DEMO_TOKENS.split("").map(
+  (identifier, idx) => ({
+    type: "identifier",
+    length: 1,
+    pos: 1 + idx,
+    line: 1,
+    text: identifier,
+  })
+);
+
+const createDemoReader = () => {
+  const myStream = [...DEMO_TOKEN_STREAM];
+  function scan(): Token {
+    const token = myStream.shift();
+    if (token) {
+      return token;
+    } else {
+      return {
+        type: "end",
+        length: 0,
+        pos: DEMO_TOKENS.length + 1,
+        line: 1,
+      };
+    }
+  }
+  return scan;
+};
+
+describe("Scanner", () => {
+  it(`Scans`, () => {
+    const scanner = new Scanner(createDemoReader());
+
+    DEMO_TOKENS.split("").forEach((t, idx) => {
+      expect(scanner.current().pos).toBe(idx + 1);
+      expect(scanner.current().type).toBe("identifier");
+      scanner.readNext();
+    });
+    expect(scanner.current().type).toBe("end");
+  });
+  it(`Scans with one control point`, () => {
+    const scanner = new Scanner(createDemoReader());
+    expect(scanner.current().pos).toBe(1);
+
+    scanner.makeControlPoint();
+    scanner.clearControlPoint();
+
+    expect(() => scanner.clearControlPoint()).toThrow();
+
+    expect(scanner.current().pos).toBe(1);
+
+    scanner.makeControlPoint();
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(2);
+    scanner.clearControlPoint();
+    expect(scanner.current().pos).toBe(2);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(3);
+
+    scanner.makeControlPoint();
+    expect(scanner.current().pos).toBe(3);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(4);
+    scanner.rollbackControlPoint();
+    expect(scanner.current().pos).toBe(3);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(4);
+
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(5);
+
+    scanner.makeControlPoint();
+    expect(scanner.current().pos).toBe(5);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(6);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(7);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(8);
+    scanner.rollbackControlPoint();
+    expect(scanner.current().pos).toBe(5);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(6);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(7);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(8);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(9);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(10);
+  });
+
+  it(`Scans with two empty control points `, () => {
+    const scanner = new Scanner(createDemoReader());
+
+    scanner.makeControlPoint();
+    scanner.makeControlPoint();
+
+    scanner.clearControlPoint();
+    scanner.clearControlPoint();
+    expect(scanner.current().pos).toBe(1);
+
+    scanner.makeControlPoint();
+    scanner.makeControlPoint();
+
+    scanner.rollbackControlPoint();
+    scanner.rollbackControlPoint();
+    expect(scanner.current().pos).toBe(1);
+  });
+  it(`Scans with two control points, each rolledback`, () => {
+    const scanner = new Scanner(createDemoReader());
+
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(2);
+    scanner.makeControlPoint();
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(3);
+    scanner.makeControlPoint();
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(4);
+
+    scanner.rollbackControlPoint();
+    expect(scanner.current().pos).toBe(3);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(4);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(5);
+
+    scanner.rollbackControlPoint();
+    expect(scanner.current().pos).toBe(2);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(3);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(4);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(5);
+  });
+
+  it(`Scans with two control points, first cleared, second rolledback`, () => {
+    const scanner = new Scanner(createDemoReader());
+    scanner.readNext();
+
+    scanner.makeControlPoint();
+    scanner.readNext();
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(4);
+
+    scanner.makeControlPoint();
+    scanner.readNext();
+    scanner.rollbackControlPoint();
+    expect(scanner.current().pos).toBe(4);
+
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(5);
+
+    scanner.clearControlPoint();
+    expect(scanner.current().pos).toBe(5);
+  });
+
+  it(`Scans with two control points, first rolled back, second cleared`, () => {
+    const scanner = new Scanner(createDemoReader());
+    scanner.readNext();
+
+    scanner.makeControlPoint();
+    scanner.readNext();
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(4);
+
+    scanner.makeControlPoint();
+    scanner.readNext();
+    scanner.clearControlPoint();
+    expect(scanner.current().pos).toBe(5);
+
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(6);
+
+    scanner.rollbackControlPoint();
+    expect(scanner.current().pos).toBe(2);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(3);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(4);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(5);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(6);
+    scanner.readNext();
+    expect(scanner.current().pos).toBe(7);
+  });
+});
--- scanner.ts
+++ scanner.ts
@@ -1,8 +1,6 @@
 import { createScanner, Token } from "./scanner.func";
 
 export class Scanner {
-  private scan: () => Token;
-
   private currentToken: Token;
   constructor(private scanner: () => Token) {
     this.currentToken = this.scanner();
@@ -12,7 +10,49 @@ export class Scanner {
     return this.currentToken;
   }
 
+  private pushedBackTokens: Token[] = [];
+
   readNext() {
-    this.currentToken = this.scanner();
+    const pushedBackToken = this.pushedBackTokens.shift();
+
+    const currentToken = pushedBackToken ? pushedBackToken : this.scanner();
+
+    this.controlPoints.forEach((controlPoint) =>
+      controlPoint.push(currentToken)
+    );
+    this.currentToken = currentToken;
+  }
+
+  private controlPoints: Token[][] = [];
+
+  public makeControlPoint() {
+    this.controlPoints.push([this.currentToken]);
+  }
+  public clearControlPoint() {
+    const dataInControlPoint = this.controlPoints.pop();
+    if (!dataInControlPoint) {
+      throw new Error(`No saved control point!`);
+    }
+  }
+  public rollbackControlPoint() {
+    const dataInControlPoint = this.controlPoints.pop();
+    if (!dataInControlPoint) {
+      throw new Error(`No saved control point!`);
+    }
+
+    // Clear other control points
+    this.controlPoints.forEach((controlPoint) => {
+      dataInControlPoint.forEach(() => controlPoint.pop());
+    });
+
+    dataInControlPoint.forEach((token) => this.pushedBackTokens.push(token));
+    this.readNext();
   }
 }
+
+/*
+
+  *   *    
+a b c d e f g 
+
+*/
