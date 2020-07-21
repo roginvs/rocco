c404f024dfdae2c08c48206c5133045d0f901cdd
Vasilii Rogin <vasilii@giosg.com>
2020-07-21 19:54:37 +0300

    Working on parser

---
+++ parser.func.test.ts
@@ -0,0 +1,59 @@
+import { createParser } from "./parser.func";
+import { Scanner } from "./scanner";
+import { createScannerFunc } from "./scanner.func";
+import { assert } from "console";
+
+function parse(str: string) {
+  return createParser(new Scanner(createScannerFunc(str)));
+}
+
+describe("Parser test", () => {
+  it("Reads primary expression number int", () => {
+    const parser = parse("123");
+    const node = parser.readPrimaryExpression();
+    expect(node).toMatchObject({
+      type: "const",
+      subtype: "int",
+      value: 123,
+    });
+  });
+
+  it("Reads primary expression number float", () => {
+    const parser = parse("123.5");
+    const node = parser.readPrimaryExpression();
+    expect(node).toMatchObject({
+      type: "const",
+      subtype: "float",
+      value: 123.5,
+    });
+  });
+
+  it("Reads primary expression char", () => {
+    const parser = parse("'a'");
+    const node = parser.readPrimaryExpression();
+    expect(node).toMatchObject({
+      type: "const",
+      subtype: "char",
+      value: "a".charCodeAt(0),
+    });
+  });
+
+  it("Reads primary expression identifier", () => {
+    const parser = parse("asdf");
+    const node = parser.readPrimaryExpression();
+    expect(node).toMatchObject({
+      type: "identifier",
+      value: "asdf",
+    });
+  });
+
+  it("Reads postfix expression", () => {
+    const parser = parse("asd[2]");
+    const node = parser.readPrimaryExpression();
+    console.info(node);
+    expect(node).toMatchObject({
+      type: "subscript operator",
+      target: null,
+    });
+  });
+});
