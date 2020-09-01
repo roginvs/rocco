8c59b8da9b9eaefad1d5cc85b2df139c993815a5
Vasilii Rogin <vasilii@giosg.com>
2020-09-01 20:30:55 +0300

    Update casting example

--- test/casting.c
+++ test/casting.c
@@ -7,12 +7,18 @@ gcc -Wall -o casting.bin casting.c
 long int i;
 int *p;
 
+int func(int x)
+{
+  return x + 1;
+}
+
 int main()
 {
   unsigned char c;
   // No casting needed
   c = i;
   float f = i;
+  i = f;
 
   // Casting example
   p = (int *)i;
@@ -26,6 +32,13 @@ int main()
 
   p = (int *)((long int *)(void *)&i + 1);
 
+  // A pointer to function
+  int (*pf)(int xxx) = &func;
+
+  // Casting to pointer to function
+  pf = (int (*)(int))p;
+
+  // Suppress "Unused variables" warning in gcc
   c = c + 1;
   f = f + 1;
   return 0;
