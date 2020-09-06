5be8202a44ce650e15ed6bd84e8f6b6d8a462630
Vasilii Rogin <vasilii@giosg.com>
2020-09-06 18:01:53 +0300

    Validated for loop

--- C/2.c
+++ C/2.c
@@ -14,7 +14,21 @@ void test_array(int x, int y, int z)
   // (myarr[1]) <- this is array of y elements of z chars each
 }
 
+int for_loop_1()
+{
+  int s = 100;
+  // (10 - i)  is the same as  !(i == 10)
+  for (int i = 0; 10 - i; i = i + 1)
+  {
+    s = s + i;
+  };
+  return s;
+}
+
+#include <stdio.h>
+
 int main()
 {
+  printf("for_loop_1 = %i\n", for_loop_1());
   return 0;
 }
