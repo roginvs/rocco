294fa5123506bd9890d6f1906d90a88a47fd8386
Vasilii Rogin <vasilii@giosg.com>
2020-09-08 20:00:00 +0300

    Add tests

--- emittertests/emitter4.test.ts
+++ emittertests/emitter4.test.ts
@@ -5,10 +5,13 @@ describe(`Emits and compiles`, () => {
     const d = await compile<{
       call_simple_1(x: number): number;
       call_simple_2(x: number, y: number): number;
+      call_simple_3(x: number): number;
     }>("emitter4.c");
 
     expect(d.compiled.call_simple_1(8)).toBe(17);
 
     expect(d.compiled.call_simple_2(7, 11)).toBe(7 * 2 + 11);
+
+    expect(d.compiled.call_simple_3(130)).toBe(130 + 11);
   });
 });
