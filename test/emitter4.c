0a22c10d8f59048707aa733c30f4ce7be3742409
Vasilii Rogin <vasilii@giosg.com>
2020-09-08 19:35:46 +0300

    Fixes

--- test/emitter4.c
+++ test/emitter4.c
@@ -19,13 +19,13 @@ int call_simple_2(int i, int j)
   return multiply_by_2_first_and_add_second(i, j);
 }
 
-int add_eleven(int *p)
+void add_eleven(int *p)
 {
   (*p) = (*p) + 11;
 };
 
 int call_simple_3(int i)
 {
-  add_eleven(i);
+  add_eleven(&i);
   return i;
 }
