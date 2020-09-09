9002509cd22bf17f3ece7f8c083eeb5a61c5e091
Vasilii Rogin <vasilii@giosg.com>
2020-09-09 20:21:27 +0300

    Add tests for compare operators

--- emittertests/emitter6.test.ts
+++ emittertests/emitter6.test.ts
@@ -6,6 +6,7 @@ describe(`Emits and compiles`, () => {
       compare_eq(x: number, y: number): number;
       factor(x: number): number;
       op_u(type: number, x: number, y: number): number;
+      op_s(type: number, x: number, y: number): number;
     }>("emitter6.c");
     const m = d.compiled;
 
@@ -26,11 +27,20 @@ describe(`Emits and compiles`, () => {
     expect(m.op_u(1, 10, 2)).toBe(1);
 
     expect(m.op_u(2, 1, 2)).toBe(1);
-    expect(m.op_u(2, 2, 2)).toBe(1);
+    expect(m.op_u(2, 2, 2)).toBe(0);
     expect(m.op_u(2, 10, 2)).toBe(0);
 
     expect(m.op_u(3, 1, 2)).toBe(1);
-    expect(m.op_u(3, 2, 2)).toBe(0);
+    expect(m.op_u(3, 2, 2)).toBe(1);
     expect(m.op_u(3, 10, 2)).toBe(0);
+
+    expect(m.op_u(4, 10, 10)).toBe(1);
+    expect(m.op_u(4, 10, 3)).toBe(0);
+    expect(m.op_u(5, 10, 10)).toBe(0);
+    expect(m.op_u(5, 10, 3)).toBe(1);
+    expect(m.op_u(6, 0x00ff00ff, 0x00fa2302)).toBe(0x00ff00ff ^ 0x00fa2302);
+
+    expect(m.op_u(0, 0xffffffff, 2)).toBe(1);
+    expect(m.op_s(0, 0xffffffff, 2)).toBe(0);
   });
 });
