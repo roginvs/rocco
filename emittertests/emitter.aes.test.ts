37bfa441857757966cadf2b4aba7cd1fae5de36c
Vasilii Rogin <vasilii@giosg.com>
2020-09-09 21:44:39 +0300

    Add tests for galois

--- emittertests/emitter.aes.test.ts
+++ emittertests/emitter.aes.test.ts
@@ -7,11 +7,21 @@ describe(`Emits and compiles`, () => {
       _init_inverse_bits_table(): void;
       poly_multiple_inversed(a: number, b: number): number;
       poly_multiple(a: number, b: number): number;
+      _get_bezout_identity(
+        a: number,
+        b: number,
+        a_have_highest_bit: number,
+        x_addr: number,
+        y_addr: number
+      ): number;
+      get_inverse_element(a: number): number;
     }>("emitter.aes.c");
 
     d.compiled._init_inverse_bits_table();
 
     const inverse_addr = d.compiled._inverse_bits_address();
+
+    // Check https://github.com/roginvs/test_crypto/blob/master/galois.test.c
 
     expect(d.mem8[inverse_addr + 0x00]).toBe(0x00);
     expect(d.mem8[inverse_addr + 0b00000001]).toBe(0b10000000);
@@ -27,6 +37,13 @@ describe(`Emits and compiles`, () => {
       )
     ).toBe(inverse_bits(0b10111010));
 
-    expect(d.compiled.poly_multiple(0x57, 0x13)).toBe(0xfe);
+    for (let i = 0xff; i > 0; i--) {
+      const target = i;
+      const result = d.compiled.get_inverse_element(target);
+
+      const multiplication = d.compiled.poly_multiple(target, result);
+
+      expect(multiplication).toBe(1);
+    }
   });
 });
