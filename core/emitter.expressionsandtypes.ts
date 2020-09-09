e1414a25a75110ef1c1b81216bf96c9b96110672
Vasilii Rogin <vasilii@giosg.com>
2020-09-09 20:40:12 +0300

    Remove stub

--- core/emitter.expressionsandtypes.ts
+++ core/emitter.expressionsandtypes.ts
@@ -1072,17 +1072,7 @@ export function createExpressionAndTypes(
         ],
       };
     } else if (expression.type === "conditional expression") {
-      return {
-        address: null,
-        value: () => ["i32.const 2"],
-        type: {
-          type: "arithmetic",
-          arithmeticType: "int",
-          const: true,
-          signedUnsigned: null,
-        },
-        staticValue: null,
-      };
+      error(expression, "TODO ME conditional expression");
     }
 
     error(expression, `TODO other expressionInfo for type=${expression.type}`);
