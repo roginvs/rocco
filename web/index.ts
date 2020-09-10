60d7f95b6c33bb3acb333da839bc4c3fb44c8dc3
Vasilii Rogin <vasilii@giosg.com>
2020-09-10 18:21:50 +0300

    Add readFileSync into web

--- web/index.ts
+++ web/index.ts
@@ -1,1 +1,7 @@
-console.info("Lol");
+import { readFileSync } from "fs";
+
+const aesCode = readFileSync(__dirname + "/../test/emitter.aes.c").toString();
+const crc32Code = readFileSync(
+  __dirname + "/../test/emitter.crc32.c"
+).toString();
+// wow
