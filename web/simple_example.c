ecdb0991f39d80369e2d21ff1a122c9166921b10
Vasilii Rogin <vasilii@giosg.com>
2020-09-11 09:23:53 +0300

    Update simple example

--- web/simple_example.c
+++ web/simple_example.c
@@ -5,5 +5,11 @@ int func1(int i, int j)
   {
     k << 1;
   }
+  else if (i > 100)
+  {
+    char (*(scary_array_of_array_of_pointers_to_function[10][20]))(int, char);
+    scary_array_of_array_of_pointers_to_function[2][4] = (void *)0;
+  }
+
   return k;
 }
